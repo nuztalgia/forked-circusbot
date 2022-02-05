e61b4d3a560f94c4e0b79404b56d3d1d2bd62105
Cad <halifaxprogrammer@gmail.com>
2022-02-05 14:18:21 -0400

    Cleanup some of the direct emoji references

--- src/events/embeds.ts
+++ src/events/embeds.ts
@@ -2,6 +2,7 @@ import { DiscordAPIError, Message, MessageEmbed } from "discord.js";
 import { client } from '../client';
 import { log } from "../utils/logging";
 import { saveEvents } from "./persistence";
+import { EMOJI_DPS, EMOJI_DPS_SUB, EMOJI_HEAL, EMOJI_HEAL_SUB, EMOJI_TANK, EMOJI_TANK_SUB } from "./reactions";
 
 export async function updateEventEmbeds(event: CircusEvent) {
     const embed = createEventEmbed(event);
@@ -31,15 +32,15 @@ export async function updateEventEmbeds(event: CircusEvent) {
                 return;
             }
 
-            if (event.signup_status === 'open' && !(msg.reactions.cache.get('❤️')?.count || 0 > 0)) {
+            if (event.signup_status === 'open' && !(msg.reactions.cache.get(EMOJI_DPS_SUB)?.count || 0 > 0)) {
                 log('debug', `Event ${event.id} is open to sign-ups, adding sign-up reactions (message: ${msg.id})`);
-                await msg.react('<:tank:933048000727629835>');
-                await msg.react('<:heal:933048000740229140>');
-                await msg.react('<:dps:933048000866033774');
-                await msg.react('💙');
-                await msg.react('💚');
-                await msg.react('❤️');
-            } else if (event.signup_status === 'closed' && (msg.reactions.cache.get('❤️')?.count || 0) > 0) {
+                await msg.react(EMOJI_TANK);
+                await msg.react(EMOJI_HEAL);
+                await msg.react(EMOJI_DPS);
+                await msg.react(EMOJI_TANK_SUB);
+                await msg.react(EMOJI_HEAL_SUB);
+                await msg.react(EMOJI_DPS_SUB);
+            } else if (event.signup_status === 'closed' && (msg.reactions.cache.get(EMOJI_DPS_SUB)?.count || 0) > 0) {
                 log('debug', `Event ${event.id} is closed to sign-ups, removing sign-up reactions (message: ${msg.id})`);
                 await msg.reactions.removeAll();
             }
@@ -57,7 +58,6 @@ export function createEventEmbed(event: CircusEvent) {
     const healer_subs = (Object.values(event.signups.healer_subs).length > 0 ? "💚 " : "") + Object.values(event.signups.healer_subs).map(x => x.substring(0, 20)).join("\n💚 ") || '\u200b';
     const dps_subs = (Object.values(event.signups.dps_subs).length > 0 ? "❤️ " : "") + Object.values(event.signups.dps_subs).map(x => x.substring(0, 20)).join("\n❤️ ") || '\u200b';
 
-
     if (!event.time?.match(/ [A-Z]{3}$/)) {
         event.time += ' EST';
     }
@@ -65,9 +65,9 @@ export function createEventEmbed(event: CircusEvent) {
     const description = `:calendar_spiral: ${event.date}⠀⠀⠀⠀:alarm_clock: ${event.time}\n\n` + 
         (event.description ? event.description + "\n\n" : "") +
         `**Requirements:**\n` + 
-        `<:tank:933048000727629835>  ${event.role_requirements.tank}\n` +
-        `<:heal:933048000740229140>  ${event.role_requirements.healer}\n` +
-        `<:dps:933048000866033774>  ${event.role_requirements.dps}\n\n` +
+        `${EMOJI_TANK}  ${event.role_requirements.tank}\n` +
+        `${EMOJI_HEAL}  ${event.role_requirements.healer}\n` +
+        `${EMOJI_DPS}  ${event.role_requirements.dps}\n\n` +
         `You may select up to one main role and up to three sub roles by using the reactions below. ` + 
         `Clicking the same reaction a second time will cancel your sign-up for that role. ` + 
         `Please make sure you meet the requirements for your role before signing up!\n⠀\n`;
@@ -76,21 +76,21 @@ export function createEventEmbed(event: CircusEvent) {
         .setColor("#0099ff")
         .setDescription(description)
         .addFields(
-            { name: `<:tank:933048000727629835> Tanks (${Object.values(event.signups.tanks).length}/${event.role_limits.tank})`, value: tank_signups, inline: true },
-            { name: `<:heal:933048000740229140> Healers (${Object.values(event.signups.healers).length}/${event.role_limits.healer})`, value: healer_signups, inline: true },
-            { name: `<:dps:933048000866033774> DPS (${Object.values(event.signups.dps).length}/${event.role_limits.dps})`, value: dps_signups, inline: true },
-            { name: `💙 Tank Subs (${Object.values(event.signups.tank_subs).length})`, value: tank_subs, inline: true },
-            { name: `💚 Healer Subs (${Object.values(event.signups.healer_subs).length})`, value: healer_subs, inline: true },
-            { name: `❤️ DPS Subs (${Object.values(event.signups.dps_subs).length})`, value: dps_subs, inline: true }
+            { name: `${EMOJI_TANK} Tanks (${Object.values(event.signups.tanks).length}/${event.role_limits.tank})`, value: tank_signups, inline: true },
+            { name: `${EMOJI_HEAL} Healers (${Object.values(event.signups.healers).length}/${event.role_limits.healer})`, value: healer_signups, inline: true },
+            { name: `${EMOJI_DPS} DPS (${Object.values(event.signups.dps).length}/${event.role_limits.dps})`, value: dps_signups, inline: true },
+            { name: `${EMOJI_TANK_SUB} Tank Subs (${Object.values(event.signups.tank_subs).length})`, value: tank_subs, inline: true },
+            { name: `${EMOJI_HEAL_SUB} Healer Subs (${Object.values(event.signups.healer_subs).length})`, value: healer_subs, inline: true },
+            { name: `${EMOJI_DPS_SUB} DPS Subs (${Object.values(event.signups.dps_subs).length})`, value: dps_subs, inline: true }
         )
         .setFooter({ text: `${event.signup_status == 'open' ? '📖' : '🙈'} Sign-ups are currently ${event.signup_status}  •  Event ID: ${event.id}`});
 
     if (event.title.match(/Pub/)) {
-        embed.setAuthor({ name: event.title || 'Untitled Event', iconURL: 'https://cdn.discordapp.com/emojis/740147910435405864.webp?size=96&quality=lossless' });
+        embed.setAuthor({ name: event.title, iconURL: 'https://cdn.discordapp.com/emojis/740147910435405864.webp?size=96&quality=lossless' });
     } else if (event.title?.match(/(Imp|Empire)/)) {
-        embed.setAuthor({ name: event.title || 'Untitled Event', iconURL: 'https://cdn.discordapp.com/emojis/740147893289091122.webp?size=96&quality=lossless' });
+        embed.setAuthor({ name: event.title, iconURL: 'https://cdn.discordapp.com/emojis/740147893289091122.webp?size=96&quality=lossless' });
     } else {
-        embed.setAuthor({ name: event.title || 'Untitled Event', iconURL: 'https://cdn.discordapp.com/emojis/933190190376288256.webp?size=96&quality=lossless' });
+        embed.setAuthor({ name: event.title, iconURL: 'https://cdn.discordapp.com/emojis/933190190376288256.webp?size=96&quality=lossless' });
     }
 
     return embed;
---
+++ src/events/reactions.ts
@@ -0,0 +1,6 @@
+export const EMOJI_TANK = '<:tank:933048000727629835>';
+export const EMOJI_HEAL = '<:heal:933048000740229140>';
+export const EMOJI_DPS = '<:dps:933048000866033774>';
+export const EMOJI_TANK_SUB = '💙';
+export const EMOJI_HEAL_SUB = '💚';
+export const EMOJI_DPS_SUB = '❤️';
