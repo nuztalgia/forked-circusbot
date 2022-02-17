81c61150bcad89d244d0a2e14a4565ddc24e8964
Cad <halifaxprogrammer@gmail.com>
2022-02-17 15:23:36 -0400

    Make canned reply names case insensitive

--- src/cannedreplies/listener.ts
+++ src/cannedreplies/listener.ts
@@ -14,15 +14,15 @@ export function cannedReplyHandler(message: Message<boolean>) {
 
     // Assign a message
     if (content.includes('=')) {
-        let name = content.split('=')[0];
+        let name = content.split('=')[0].toLowerCase();
         let value = content.substring(name.length).trim().substring(1).trim().replace(/(^"|"$)/g, '');
         cannedReplies[message.guildId][name] = { value, author: message.author.tag };
         message.react('👍');
         log('info', `New canned reply added by ${message.author.tag}: ${name}`);
         savePersistentData('cannedreplies', cannedReplies);
-    } else if (cannedReplies[message.guildId].hasOwnProperty(content)) {
-        sendReply(message, EMBED_INFO_COLOR, cannedReplies[message.guildId][content].value);
+    } else if (cannedReplies[message.guildId].hasOwnProperty(content.toLowerCase())) {
+        sendReply(message, EMBED_INFO_COLOR, cannedReplies[message.guildId][content.toLowerCase()].value);
     } else {
-        sendReply(message, EMBED_ERROR_COLOR, 'Unknown canned message. To create a new canned message, please use `=name=Your custom text here`');
+        sendReply(message, EMBED_ERROR_COLOR, 'Unknown canned message. To create a new canned message, please use the following syntax (anyone can create canned messages):\n\n`=name=Your custom text here`\n\nThen you can print the content of the canned reply using `=name` in any channel with this bot in it.');
     }
 }
