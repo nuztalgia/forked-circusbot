11a74ca9ea0fe63ff422d8042db5ab17538486a5
Cad <halifaxprogrammer@gmail.com>
2022-03-02 14:42:37 -0400

    Show emojis for canned replies at a bigger size

--- src/cannedreplies/listener.ts
+++ src/cannedreplies/listener.ts
@@ -10,6 +10,9 @@ export function saveCannedReplies() {
 export function renderCannedReply(reply: any) {
     if (reply.hasOwnProperty('url')) {
         return new MessageEmbed().setDescription(reply.value || '').setImage(reply.url);
+    } else if (reply.value.trim().match(/^<:.*?:([0-9]+)>$/)) {
+        const emoji = reply.value.trim().match(/^<:.*?:([0-9]+)>$/);
+        return new MessageEmbed().setThumbnail(`https://cdn.discordapp.com/emojis/${emoji[1]}.png?size=96&quality=lossless`);
     } else {
         return new MessageEmbed().setDescription(reply.value);
     }
