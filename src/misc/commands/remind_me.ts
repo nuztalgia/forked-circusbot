3ad3611a3898ad8f9caac7584768677f96634a0e
Cad <halifaxprogrammer@gmail.com>
2022-03-08 12:31:00 -0400

    usability improvements to remindme command

--- src/misc/commands/remind_me.ts
+++ src/misc/commands/remind_me.ts
@@ -1,8 +1,25 @@
-import { Message, TextChannel, User } from 'discord.js';
+import { Message, MessageEmbed, TextChannel } from 'discord.js';
 import { client } from '../../client';
-import { registerCommand, getRandomInt, parseCommand, sendReply, EMBED_ERROR_COLOR, EMOJI_ERROR, EMBED_SUCCESS_COLOR, loadPersistentData, log, getFormattedDate, savePersistentData } from '../../utils';
+import { registerCommand, parseCommand, sendReply, EMBED_ERROR_COLOR, loadPersistentData, log, getFormattedDate, savePersistentData, EMBED_DMM_COLOR, makeError, arrayRandom } from '../../utils';
 
 let reminders = loadPersistentData('reminders', []);
+
+const RANDOM_REASONS = [
+    'Maybe you left the fridge running?',
+    'Maybe you forgot to turn off your oven?',
+    'Maybe you have a raid?',
+    'Maybe it\'s time to take a break?',
+    'Maybe it\'s time to let the dogs out?',
+    'Maybe it\'s time to feed your pet fish?',
+    'Maybe it\'s time to water the lawn?',
+    'Maybe it\'s time to drink some more water?',
+    'Maybe it\'s time to check the oven',
+    'Maybe it\'s time to get some more coffee?',
+    'Maybe it\'s time to go outside for some fresh air?',
+    'Maybe it\'s time to walk the dogs?',
+    'Maybe it\'s time to take your vitamins?',
+    'Maybe it\'s time to send that email?',
+];
 
 client.on('ready', () => {
     reminders.forEach(reminder => scheduleReminder(reminder));
@@ -13,7 +30,15 @@ function scheduleReminder(reminder: any) {
 
     setTimeout(async () => {
         const channel = await client.channels.fetch(reminder.channel) as TextChannel;
-        channel.send({ content: `<@${reminder.remindee}> ${reminder.reminder}` })
+        const description = reminder.reminder ? reminder.reminder : `No reminder reason was specified. ${arrayRandom(RANDOM_REASONS)}`;
+        const embed = new MessageEmbed()
+            .setColor(EMBED_DMM_COLOR)
+            .setDescription(`${description}\n\nI hope I\'ve jogged your memory <:potatoAngel:925130155507191948>`)
+            .setFooter({ text: '⏲️ To schedule a reminder, use the !remindme command' });
+        const content = `<@${reminder.remindee}> ${reminder.remindedBy === reminder.remindee ? 'You' : reminder.remindedByTag} asked me to remind you at this time with the following message:`;
+
+        channel.send({ content, embeds: [embed] })
+
         reminders = reminders.filter(x => x.id !== reminder.id);
         savePersistentData('reminders', reminders);
     }, Date.parse(reminder.remindAt) - Date.now());
@@ -22,25 +47,28 @@ function scheduleReminder(reminder: any) {
 registerCommand('remind_me', ['remindme', 'remind'], message => {
     if (!(message instanceof Message)) return;
 
-    let [user, remindTime, remindUnit, reminder] = parseCommand(message, /(<.*?> )?(([0-9]+) (?:hour|minute|day|week|month)s?|[0-9]{1,2}:[0-9]{2} ?(?:AM|PM))(.*)/i);
+    let [user, remindTime, remindUnit, reminder] = parseCommand(message, /(<.*?> )?(([0-9]+) ?(?:s|seconds?|m|minutes?|h|hours?|d|days?|w|weeks?|m|months?)|[0-9]{1,2}:[0-9]{2} ?(?:AM|PM))(.*)/i);
     
     let remindee = user ? message.mentions.users.first() : message.author;
 
     if (!remindee || !remindTime) {
+        sendReply(message, EMBED_ERROR_COLOR, makeError('Incorrect usage. Please specify a time and reason for the reminder, for example:\n\n```\n!remindme 3 hours Login for raid\n```\n'));
         return;
     }
 
     let remindAt = new Date();
 
-    if (remindTime.includes('minute')) {
+    if (remindTime.includes('second') || remindTime.match(/[0-9] ?s$/)) {
+        remindAt.setSeconds(remindAt.getSeconds() + parseInt(remindUnit));
+    } else if (remindTime.includes('minute') || remindTime.match(/[0-9] ?m$/)) {
         remindAt.setMinutes(remindAt.getMinutes() + parseInt(remindUnit));
-    } else if (remindTime.includes('hour')) {
+    } else if (remindTime.includes('hour') || remindTime.match(/[0-9] ?h$/)) {
         remindAt.setHours(remindAt.getHours() + parseInt(remindUnit));
-    } else if (remindTime.includes('day')) {
+    } else if (remindTime.includes('day') || remindTime.match(/[0-9] ?d$/)) {
         remindAt.setHours(remindAt.getHours() + parseInt(remindUnit) * 24);
-    } else if (remindTime.includes('week')) {
+    } else if (remindTime.includes('week') || remindTime.match(/[0-9] ?w$/)) {
         remindAt.setHours(remindAt.getHours() + parseInt(remindUnit) * 24 * 7);
-    } else if (remindTime.includes('month')) {
+    } else if (remindTime.includes('month') || remindTime.match(/[0-9] ?m$/)) {
         remindAt.setMonth(remindAt.getMonth() + parseInt(remindUnit));
     } else {
         let scheduledTime = remindAt.getFullYear() + "-" + ('0' + (remindAt.getMonth() + 1)).slice(-2) + "-" + ('0' + remindAt.getDate()).slice(-2) + ' ' + remindTime + ' EST';
@@ -51,9 +79,10 @@ registerCommand('remind_me', ['remindme', 'remind'], message => {
         id: message.id,
         channel: message.channel.id,
         remindedBy: message.author.id,
+        remindedByTag: message.author.tag,
         remindee: remindee.id,
         remindAt: getFormattedDate(remindAt),
-        reminder: reminder ? `${message.author.id === remindee.id ? 'You' : message.author.tag} asked me to remind you at this time with the following message: ${reminder}` : `${message.author.id === remindee.id ? 'You' : message.author.tag} asked me to remind you at this time, but didn\'t specify a reason. I hope I\'ve jogged your memory <:potatoAngel:925130155507191948>`,
+        reminder: reminder,
     };
 
     reminders.push(newReminder)
