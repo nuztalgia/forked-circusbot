cd4ee96bbc1e6cf9194bf51dbfff92db837263eb
Cad <halifaxprogrammer@gmail.com>
2022-08-30 10:59:37 -0300

    Add comments for copyemote command

--- src/admin/commands/copyemote.ts
+++ src/admin/commands/copyemote.ts
@@ -22,22 +22,30 @@ bot.registerCommand('copyemote', ['emote', 'addemote'], async message => {
     let name = '';
     let targetMsg = message;
 
+    // Allow the copyemote command to be used by passing in an attachment or emoji from another server, or by replying
+    // to a message that is an attachment or emoji from another server (common use case is for someone to post a cool emoji
+    // in chat and we want to steal it for ourselves).
     if (message.type === 'REPLY') {
         targetMsg = await message.fetchReference();
     }
 
     if (targetMsg.attachments.size > 0) {
+        // If the message has an attachment, get the name from the command or attachment filename (e.g. `!copyemote rooPing` will
+        // use rooPing as the name). URL is just the attachment CDN url
         url = targetMsg.attachments.first()?.url || '';
         name = emote.trim() || targetMsg.attachments.first()?.name?.replace(/\.[a-z0-9]+$/i, '') || 'untitled';
     } else {
+        // Command was called against an emoji so we get the emoji ID and name (emoji syntax is like <:name:long_id_string>)
         let emoji = message.type === 'REPLY' ? targetMsg.content : emote;
         let m = emoji.match(/<(a?):([^:]+):([0-9]+)>/i);
 
+        // Message didn't contain an emoji to copy
         if (!m) {
             bot.replyTo(message, EMBED_ERROR_COLOR, makeError("No emoji detected"));
             return;
         }
 
+        // This is how to generate a URL for an emoji
         url = `https://cdn.discordapp.com/emojis/${m[3]}.${m[1] === 'a' ? 'gif' : 'png'}?size=4096&quality=lossless`;
         name = m[2];
 
@@ -49,13 +57,16 @@ bot.registerCommand('copyemote', ['emote', 'addemote'], async message => {
     }
     
     try {
-        console.log(url, name);
+        log('debug', `Attempting to add new emote (url: ${url}, name :${name})`);
         const newEmoji = await message.guild.emojis.create(url, name.replace(/:/g, ''));
         log('info', `${message.author.tag} has added a new emote (${url}, name: ${newEmoji.name}, id: ${newEmoji.id})`);
+
+        // Need a slight timeout after adding the emote or it won't load in the resulting message
         setTimeout(() => {
             bot.replyTo(message, EMBED_SUCCESS_COLOR, `Your new emote has been added <${url.includes('.gif') ? 'a' : ''}:${newEmoji.name}:${newEmoji.id}>`);
         }, 100);
     } catch (err) {
+        // Typical failures is only if we're out of emoji slots, or the attachment is too big
         bot.replyTo(message, EMBED_ERROR_COLOR, makeError("Failed to create emoji: " + err));
     }
 });
